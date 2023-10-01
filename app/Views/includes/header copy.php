5f57928ec982cc9ae5963889fe2a5715bb21808e
db-hasan <infoalihasanbd@gmail.com>
2023-10-01 13:13:33 +0600

    pudt

--- app/Views/includes/header copy.php
+++ app/Views/includes/header copy.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
     <title>Document</title>
+    <style>
+        .nav-link.active {
+            color: rgb(17, 236, 39) !important;
+        }
+    </style>
 </head>
 
 <body>
